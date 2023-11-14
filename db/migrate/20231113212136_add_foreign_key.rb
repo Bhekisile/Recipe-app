01f6b4996ce38789a2f4ddf9c0013e7f2ed80f7b
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-11-14 12:09:51 +0200

    Add unit tests for food and recipe_food models

--- db/migrate/20231113212136_add_foreign_key.rb
+++ db/migrate/20231113212136_add_foreign_key.rb
@@ -1,6 +1,6 @@
-# class AddForeignKey < ActiveRecord::Migration[7.1]
-#   def change
-#     add_foreign_key :foods, :users, column: :user_id
-#     add_foreign_key :recipe_foods, :recipes, column: :recipe_id
-#   end
-# end
+class AddForeignKey < ActiveRecord::Migration[7.1]
+  def change
+    add_foreign_key :foods, :users, column: :user_id
+    add_foreign_key :recipe_foods, :recipes, column: :recipe_id
+  end
+end
--- spec/models/food_spec.rb
+++ spec/models/food_spec.rb
@@ -2,10 +2,32 @@ require 'rails_helper'
 
 RSpec.describe 'Food', type: :request do
   let(:food) { Food.create(name: 'apple', measurement_unit: 'grams', price: 5) }
-  # before { food.save }
+  let(:user) { User.create(name: 'Joy') }
 
   it 'has many Recipe-foods' do
     food = Food.reflect_on_association(:recipe_foods)
     expect(food.macro).to eq :has_many
+  end
+
+  it 'belongs to user' do
+    food = Food.reflect_on_association(:user)
+    expect(food.macro).to eq(:belongs_to)
+  end
+
+  describe 'Validations' do
+    it 'name should be present' do
+      food.name = nil
+      expect(food).to_not be_valid
+    end
+
+    it 'measurement_unit should be present' do
+      food.measurement_unit = nil
+      expect(food).to_not be_valid
+    end
+
+    it 'price should be present' do
+      food.price = nil
+      expect(food).to_not be_valid
+    end
   end
 end
