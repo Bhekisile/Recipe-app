291c570f3e03476c27a1e209744ba155cf08f39a
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-11-18 19:19:21 +0200

    Edit yaml file

--- config/puma.rb
+++ config/puma.rb
@@ -17,21 +17,20 @@ threads min_threads_count, max_threads_count
 #   worker_count = Integer(ENV.fetch("WEB_CONCURRENCY") { Concurrent.physical_processor_count })
 #   workers worker_count if worker_count > 1
 # end
-workers ENV.fetch("WEB_CONCURRENCY") { 4 }
 
 # Specifies the `worker_timeout` threshold that Puma will use to wait before
 # terminating a worker in development environments.
-# worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
-preload_app!
+worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 environment ENV.fetch("RAILS_ENV") { "development" }
-
-# Specifies the `pidfile` that Puma will use.
 pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
+# Specifies the `pidfile` that Puma will use.
+workers ENV.fetch("WEB_CONCURRENCY") { 4 }
+preload_app!
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
