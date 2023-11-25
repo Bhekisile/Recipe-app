246b5641f6de67bfdfb7f7cbb501c7e24c14e9cb
Bhekisile <111288560+Bhekisile@users.noreply.github.com>
2023-11-25 15:17:51 +0200

    Edit add user_id to recipes

--- db/migrate/20231114201345_add_user_id_to_recipes.rb
+++ db/migrate/20231114201345_add_user_id_to_recipes.rb
@@ -1,5 +1,5 @@
-class AddUserIdToRecipes < ActiveRecord::Migration[7.1]
-  def change
-    add_reference :recipes, :user, null: false, foreign_key: true
-  end
-end
+# class AddUserIdToRecipes < ActiveRecord::Migration[7.1]
+#   def change
+#     add_reference :recipes, :user, null: false, foreign_key: true
+#   end
+# end
