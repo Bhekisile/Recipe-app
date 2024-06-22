ce3aa774a7338ebd4661e046bfebf342945114ac
Bhekisile <bhekisilejozi@gmail.com>
2024-06-22 08:26:23 +0200

    Create procfile

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,8 @@ Rails.application.configure do
 
   # Enable server timing
   config.server_timing = true
+  
+  config.host<<"recipe-app-production.up.railway.app"
 
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
---
+++ lib/tasks/fake-assets.rails
@@ -0,0 +1,6 @@
+namespace :assets do
+        desc "Should just print stuff and move on. Fake!"
+        task :precompile do 
+            puts "This is a Fake assets:precompile"
+        end
+end
