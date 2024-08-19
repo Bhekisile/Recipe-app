4903e2432fb72202dac54ddbc5e8f237627b9080
Bhekisile <bhekisilejozi@gmail.com>
2024-08-19 18:33:24 +0200

    Manipulate bootstrap for render

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -2,4 +2,5 @@
 //= link_tree ../../javascript .js
 //= link_tree ../../../vendor/javascript .js
 //= link_tree ../builds
-//= link application.css.scss
+// link application.css.scss
+//= link application.bootstrap.css
